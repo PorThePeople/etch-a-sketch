83acbe863342a9dac01b9cb920eeacc147b9c235
PorThePeople <129730998+PorThePeople@users.noreply.github.com>
2023-04-14 22:48:25 +0700

    Add function to create new grid and specify dimension

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-for (i = 1 ; i < 17 ; i++) {
+for (i = 1 ; i < 26 ; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     container.appendChild(row);
-    for (j = 1 ; j < 17 ; j++) {
+    for (j = 1 ; j < 26 ; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.style.height = '25px';
+        cell.style.width = '25px';
         row.appendChild(cell);
     }
 }
 
 const cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {cell.setAttribute('style', 'background: red;')});
-
+    cell.addEventListener('mouseover', () => {cell.style.backgroundColor = 'red'});
 });
+
+const newgrid = document.querySelector('#new');
+newgrid.addEventListener('click', () => newGrid());
+
+function newGrid() {
+    let newNumber = Number(prompt('Enter here'));
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    let side = 625 / newNumber;
+    for (i = 1 ; i < (newNumber + 1) ; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (j = 1 ; j < (newNumber + 1) ; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.height = `${side}px`;
+             cell.style.width = `${side}px`;
+            row.appendChild(cell);
+        }
+    }
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {cell.style.backgroundColor = 'red'});
+});
+    return
+};
+
